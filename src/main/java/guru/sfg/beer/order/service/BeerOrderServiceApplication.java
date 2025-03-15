8434f08bbc8bfa19286a0acbd93a0be243eb4a5e
rakes9146 <rakeshsl546@gmail.com>
2025-03-15 16:34:58 +0530

    mysql config completed

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -2,11 +2,14 @@ package guru.sfg.beer.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class BeerOrderServiceApplication {
 
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "localmysql");
+
         SpringApplication.run(BeerOrderServiceApplication.class, args);
     }
 
